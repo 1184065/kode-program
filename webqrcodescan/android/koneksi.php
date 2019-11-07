1e4d900d38bc0b364a1386629b4a0f84f636becb
Jenly Ramdan <jenly.ramdan3199@gmail.com>
2019-11-07 21:34:40 +0700

    ini web api nya pak #24

--- webqrcodescan/android/koneksi.php
+++ webqrcodescan/android/koneksi.php
@@ -1,10 +1,18 @@
 <?php
+ /*
+ penulis: Muhammad yusuf
+ website: http://www.kodingindonesia.com/
 
+ Nama Database yang telah dibuat bernama "db_android" 
+ Jika database yang kalian buat tidak sama dengan yang dibuat oleh penulis maka ganti nama database 
+ sesuai dengan nama database yang kalian buat
+ */
+ 
  //Mendefinisikan Konstanta
  define('HOST','localhost');
- define('USER','root');
- define('PASS','');
- define('DB','db_project2');
+ define('USER','id11502449_nabilajkt48lovers');
+ define('PASS','Hcmokhbuapep123');
+ define('DB','id11502449_db_project2');
  
  //membuat koneksi dengan database
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
