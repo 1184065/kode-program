2726cc3214f58fef0f6f3b48ea1bfa3fb55f383f
Jenly Ramdan <jenly.ramdan3199@gmail.com>
2020-03-17 09:34:17 +0700

    17 3 20

---
+++ app/src/main/java/com/ulf/maria/qrcodescanner/Tampil.java
@@ -0,0 +1,37 @@
+package com.ulf.maria.qrcodescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Tampil extends AppCompatActivity {
+
+    WebView webviewku;
+    WebSettings websettingku;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil);
+
+        webviewku = (WebView)findViewById(R.id.activity_main_webview);
+
+        websettingku = webviewku.getSettings();
+
+        webviewku.setWebViewClient(new WebViewClient());
+        webviewku.loadUrl("https://projectpoltekpos.000webhostapp.com/");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webviewku.canGoBack()) {
+            webviewku.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
